40846f650262cd38b541f9db64b93f4f4aa3af05
shayan26-SH <116123583+shayan26-SH@users.noreply.github.com>
2026-07-24 17:59:11 -0400

    Randoop section

---
+++ Chief_event_coordinator/Classes/RegressionTest.java
@@ -0,0 +1,9 @@
+package Chief_event_coordinator.Classes;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class })
+public class RegressionTest {
+}
+
